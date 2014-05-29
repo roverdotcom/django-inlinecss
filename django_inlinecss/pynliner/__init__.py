0c1f6b4f6b521f6c72674a10090e2462097a1dfe
Adam Bratt <adamjbratt@gmail.com>
2014-05-29 10:39:11 -0400

    Update Pyliner version

--- django_inlinecss/pynliner/__init__.py
+++ django_inlinecss/pynliner/__init__.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """Pynliner : Convert CSS to inline styles
 
-Python CSS-to-inline-styles conversion tool for HTML using BeautifulSoup and cssutils
+Python CSS-to-inline-styles conversion tool for HTML using BeautifulSoup and
+cssutils
 
-Copyright (c) 2011 Tanner Netterville
+Copyright (c) 2011-2013 Tanner Netterville
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-The generated output of this software shall not be used in a mass marketing service.
+The generated output of this software shall not be used in a mass marketing
+service.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 
-__version__ = "0.4.0"
+__version__ = "0.5.1"
 
+import re
+import urlparse
 import urllib2
 import cssutils
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Comment
 from soupselect import select
 
 
@@ -31,9 +48,13 @@ class Pynliner(object):
     stylesheet = False
     output = False
 
-    def __init__(self, log=None):
+    def __init__(self, log=None, allow_conditional_comments=False):
         self.log = log
         cssutils.log.enabled = False if log is None else True
+        self.extra_style_strings = []
+        self.allow_conditional_comments = allow_conditional_comments
+        self.root_url = None
+        self.relative_url = None
 
     def from_url(self, url):
         """Gets remote HTML page for conversion
@@ -60,13 +81,13 @@ class Pynliner(object):
         Returns self.
 
         >>> p = Pynliner()
-        >>> p.from_string('<style>h1 { color:#ffcc00; }</style><h1>Hello World!</h1>')
+        >>> p.from_string('<style>h1 {color:#ffcc00;}</style><h1>Hi</h1>')
         <Pynliner object at 0x26ac70>
         """
         self.source_string = string
         return self
 
-    def with_cssString(self, cssString):
+    def with_cssString(self, css_string):
         """Adds external CSS to the Pynliner object. Can be "chained".
 
         Returns self.
@@ -77,10 +98,7 @@ class Pynliner(object):
         >>> p.from_string(html).with_cssString(css)
         <pynliner.Pynliner object at 0x2ca810>
         """
-        if not self.style_string:
-            self.style_string = cssString + u'\n'
-        else:
-            self.style_string += cssString + u'\n'
+        self.extra_style_strings.append(css_string)
         return self
 
     def run(self):
@@ -98,7 +116,9 @@ class Pynliner(object):
         if not self.stylesheet:
             self._get_styles()
         self._apply_styles()
-        return self._get_output()
+        self._get_output()
+        self._clean_output()
+        return self.output
 
     def _get_url(self, url):
         """Returns the response content from the given url
@@ -107,8 +127,17 @@ class Pynliner(object):
 
     def _get_soup(self):
         """Convert source string to BeautifulSoup object. Sets it to self.soup.
+
+        If using mod_wgsi, use html5 parsing to prevent BeautifulSoup
+        incompatibility.
         """
-        self.soup = BeautifulSoup(self.source_string)
+        # Check if mod_wsgi is running
+        # - see http://code.google.com/p/modwsgi/wiki/TipsAndTricks
+        try:
+            from mod_wsgi import version
+            self.soup = BeautifulSoup(self.source_string, "html5lib")
+        except:
+            self.soup = BeautifulSoup(self.source_string)
 
     def _get_styles(self):
         """Gets all CSS content from and removes all <link rel="stylesheet"> and
@@ -118,7 +147,8 @@ class Pynliner(object):
         """
         self._get_external_styles()
         self._get_internal_styles()
-
+        for style_string in self.extra_style_strings:
+            self.style_string += style_string
         cssparser = cssutils.CSSParser(log=self.log)
         self.stylesheet = cssparser.parseString(self.style_string)
 
@@ -133,12 +163,11 @@ class Pynliner(object):
         link_tags = self.soup.findAll('link', {'rel': 'stylesheet'})
         for tag in link_tags:
             url = tag['href']
-            if url.startswith('http://'):
-                pass
-            elif url.startswith('/'):
-                url = self.root_url + url
-            else:
-                url = self.relative_url + url
+
+            # Convert the relative URL to an absolute URL ready to pass to urllib
+            base_url = self.relative_url or self.root_url
+            url = urlparse.urljoin(base_url, url)
+
             self.style_string += self._get_url(url)
             tag.extract()
 
@@ -203,7 +232,8 @@ class Pynliner(object):
             # for each prop_list, apply to CSSStyleDeclaration
             for prop_list in map(lambda obj: obj['props'], props):
                 for prop in prop_list:
-                    elem_style_map[elem][prop.name] = prop.value
+                    elem_style_map[elem].removeProperty(prop.name)
+                    elem_style_map[elem].setProperty(prop.name, prop.value)
 
 
         # apply rules to elements
@@ -212,7 +242,7 @@ class Pynliner(object):
                 elem['style'] = u'%s; %s' % (style_declaration.cssText.replace('\n', ' '), elem['style'])
             else:
                 elem['style'] = style_declaration.cssText.replace('\n', ' ')
-
+        
     def _get_output(self):
         """Generate Unicode string of `self.soup` and set it to `self.output`
 
@@ -220,9 +250,22 @@ class Pynliner(object):
         """
         self.output = unicode(self.soup)
         return self.output
+    
+    def _clean_output(self):
+        """Clean up after BeautifulSoup's output.
+        """
+        if self.allow_conditional_comments:
+            matches = re.finditer('(<!--\[if .+\].+?&lt;!\[endif\]-->)', self.output)
+            for match in matches:
+                comment = match.group()
+                comment = comment.replace('&gt;', '>')
+                comment = comment.replace('&lt;', '<')
+                self.output = (self.output[:match.start()] + comment +
+                               self.output[match.end():])
+
 
 def fromURL(url, log=None):
-    """Shortcut Pynliner constructor. Equivelent to:
+    """Shortcut Pynliner constructor. Equivalent to:
 
     >>> Pynliner().from_url(someURL).run()
 
@@ -231,7 +274,7 @@ def fromURL(url, log=None):
     return Pynliner(log).from_url(url).run()
 
 def fromString(string, log=None):
-    """Shortcut Pynliner constructor. Equivelent to:
+    """Shortcut Pynliner constructor. Equivalent to:
 
     >>> Pynliner().from_string(someString).run()
 
