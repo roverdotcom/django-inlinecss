72b65b91f088d1823efab0fe6a7612ea6d75951b
Jeremy Nauta <Jeremypnauta@gmail.com>
2016-12-02 12:05:52 -0700

    Configurable CSS Loader

--- django_inlinecss/conf.py
+++ django_inlinecss/conf.py
@@ -12,10 +12,26 @@ except ImportError:
 DEFAULT_ENGINE = 'django_inlinecss.engines.PynlinerEngine'
 
 
+def load_class_by_path(path):
+    i = path.rfind('.')
+    module_path, class_name = path[:i], path[i + 1:]
+    module = importlib.import_module(module_path)
+    return getattr(module, class_name)
+
+
 def get_engine():
     from django.conf import settings
     engine_path = getattr(settings, 'INLINECSS_ENGINE', DEFAULT_ENGINE)
-    i = engine_path.rfind('.')
-    module_path, class_name = engine_path[:i], engine_path[i + 1:]
-    module = importlib.import_module(module_path)
-    return getattr(module, class_name)
+    return load_class_by_path(engine_path)
+
+
+def get_css_loader():
+    from django.conf import settings
+
+    if settings.DEBUG:
+        default_css_loader = 'django_inlinecss.css_loaders.StaticFinderCSSLoader'
+    else:
+        default_css_loader = 'django_inlinecss.css_loaders.StaticPathCSSLoader'
+
+    engine_path = getattr(settings, 'INLINECSS_CSS_LOADER', default_css_loader)
+    return load_class_by_path(engine_path)
