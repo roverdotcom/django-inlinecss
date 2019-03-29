d5cab38b9cb273065511575e0468cda737d72d5a
Stephen Burrows <stephen.r.burrows@gmail.com>
2019-03-29 11:42:02 -0700

    Futurize - add imports

--- django_inlinecss/templatetags/inlinecss.py
+++ django_inlinecss/templatetags/inlinecss.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 from django import template
 
 from django.utils.encoding import smart_text
--- django_inlinecss/tests/test_templatetags.py
+++ django_inlinecss/tests/test_templatetags.py
@@ -4,6 +4,9 @@ Test the functioning of the templatetag itself.
 The actual CSS inlining displayed here is extremely simple:
 tests of the CSS selector functionality is independent.
 """
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 import os
 
 from django.test import TestCase
