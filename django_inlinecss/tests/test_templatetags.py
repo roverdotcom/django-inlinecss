b5aa9c91c464ecbc9ade48547c9d5192879c3518
Stephen Burrows <stephen.r.burrows@gmail.com>
2019-03-29 11:43:23 -0700

    Futurize - unicode literals

--- django_inlinecss/tests/test_templatetags.py
+++ django_inlinecss/tests/test_templatetags.py
@@ -7,6 +7,7 @@ tests of the CSS selector functionality is independent.
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+from __future__ import unicode_literals
 import os
 
 from django.test import TestCase
